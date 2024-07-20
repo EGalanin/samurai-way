86fdb11680cb2824061d66d2c1c29933b53923c1
EGalanin <galanin09234@mail.ru>
2024-07-20 20:05:33 +0300

    add structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <header className='header'>
+                <img src="#" alt=""/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href="#">Profile</a>
+                </div>
+                <div>
+                    <a href="#">Message</a>
+                </div>
+                <div>
+                    <a href="#">News</a>
+                </div>
+                <div>
+                    <a href="#">Music</a>
+                </div>
+                <div>
+                    <a href="#">Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="" alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My post
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
