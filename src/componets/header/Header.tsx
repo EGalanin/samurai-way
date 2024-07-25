3341c7367f73f32a207b54bfed5388c1b3e5a3ca
EGalanin <galanin09234@mail.ru>
2024-07-25 10:09:49 +0300

    add structure components

---
+++ src/componets/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <h3>Logo</h3>
+            <img src="#" alt=""/>
+        </header>
+    );
+};
+
---
+++ src/componets/profile/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://avatars.mds.yandex.net/i?id=62e4a98bd5e230054221b797c8d04b80e78831b1-4282959-images-thumbs&n=13"
+                    alt="гора"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
